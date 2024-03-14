2f2fed72db1a41f292a3627fd1c4359b40f2a9a3
dagoustic <162550425+dagoustic@users.noreply.github.com>
2024-03-14 14:33:03 +0700

    Add files via upload

---
+++ W4Ex2.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void staircase(int n) {
+    for (int i=1; i<=n; i++) {
+        for (int j=n-i; j>=1; j--) {
+            cout << " ";
+        }
+        for (int j=1; j<=i; j++) {
+            cout << "#";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    staircase(n);
+    return 0;
+}
+
+
